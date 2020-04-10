bc7102e633ab36395890aebad6c4f142c3f0cc3d
Dzmitryi Shatsila <shatilo124@gmail.com>
2020-04-10 09:41:36 +0300

    2 iteration. details

--- lesson/views.py
+++ lesson/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 # Create your views here.
+
 
 def all_materials(request):
     material_list = models.Material.objects.all()
     return render(request,
                   'materials/all_materials.html',
                   {'materials': material_list})
+
+
+def material_details(request, year, month, day, slug):
+    material = get_object_or_404(models.Material,
+                                 slug=slug,
+                                 publish__year=year,
+                                 publish__month=month,
+                                 publish__day=day)
+    return render(request,
+                  'materials/details.html',
+                  {'materials': material})
